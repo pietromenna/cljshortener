21c883d8a747e82702f38e6bc3f3c1b2828527e2
Pietro F. Menna <pietromenna@yahoo.com>
2016-03-09 23:12:05 -0300

    Added functionality to create the database table and write a link

---
+++ src/cljshortener/models/db.clj
@@ -0,0 +1,23 @@
+(ns cljshortener.models.db
+  (:require [clojure.java.jdbc :as sql])
+  (:import java.sql.DriverManager))
+
+(def db {:classname  "org.sqlite.JDBC",
+         :subprotocol   "sqlite",
+         :subname       "db.sq3"})
+
+(defn create-links-table []
+  (sql/with-connection
+    db
+    (sql/create-table
+      :links
+      [:shorturl "TEXT PRIMARY KEY"]
+      [:longurl "TEXT"])))
+
+(defn save-link [shorturl longurl]
+  (sql/with-connection 
+    db
+    (sql/insert-values
+      :links
+      [:shorturl :longurl]
+      [shorturl longurl])))
